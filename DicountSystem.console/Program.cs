18b144548df3be1e5d48d80c41822f15b4bab919
Muhammad Aftab <71002@gcslahore.edu.pk>
2025-01-14 15:54:03 +0500

    Add while loop to true for run again

--- DicountSystem.console/Program.cs
+++ DicountSystem.console/Program.cs
@@ -3,35 +3,34 @@ using premium;
 using employee;
 using Default;
 using DiscountSystem;
-Console.WriteLine("select The type of Customer");
-Console.WriteLine(" Regular");
-Console.WriteLine(" Premium");
-Console.WriteLine(" Employee");
-Console.WriteLine(" Walking");
-List<Discount<double>> list = new List<Discount<double>>()
-{
-new RegularCustomer(),
-new PremiumCustomer(),
-new Employee(),
-new DefaultCustomer()
-};
-
-string? Type = Console.ReadLine();
-string Amount = Type.ToLower() switch
-{
-    "regular" => $"Total amount to pay:{list[0].GetDiscount()}",
-    "premium" => $"Total amount to pay:{list[1].GetDiscount()}",
-    "employee" => $"Total amount to pay:{list[2].GetDiscount()}",
-    "walking" => $"Total amount to pay:{list[3].GetDiscount()}",
-    _ => "Invalid Customer Type ",
-};
-Console.WriteLine(Amount);
-//change this file
-
-
-
-
-
-
-
-
+do
+{   Console.WriteLine("select The type of Customer");
+    Console.WriteLine(" Regular");
+    Console.WriteLine(" Premium");
+    Console.WriteLine(" Employee");
+    Console.WriteLine(" Walking");
+    string? Type = Console.ReadLine();
+    List<Discount<double>> list = new List<Discount<double>>()
+    {
+        new RegularCustomer(),
+        new PremiumCustomer(),
+        new Employee(),
+        new DefaultCustomer()
+    };
+    string Amount = Type.ToLower() switch
+    {
+        "regular" => $"Total amount to pay:{list[0].GetDiscount()}",
+        "premium" => $"Total amount to pay:{list[1].GetDiscount()}",
+        "employee" => $"Total amount to pay:{list[2].GetDiscount()}",
+        "walking" => $"Total amount to pay:{list[3].GetDiscount()}",
+        _ => "Invalid Customer Type ",
+    };
+    Console.WriteLine(Amount);
+    Console.WriteLine("Do you want to calculate again: (Y/N)");
+    ConsoleKeyInfo Uchoice = Console.ReadKey();
+    if (Uchoice.Key != ConsoleKey.Y)
+    {
+        break;
+    }
+    Console.WriteLine();
+} while (true);
